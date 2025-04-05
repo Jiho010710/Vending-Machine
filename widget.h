ae486ebcea759732a8b9d71be940ab50322dcc42
jiho <wlgh010710@naver.com>
2025-04-05 00:59:02 -0400

    Update vending machine code

--- widget.h
+++ widget.h
@@ -20,20 +20,20 @@ public:
     ~Widget();
 
 private slots:
-    void on_btn10_clicked();    // 10원 버튼
-    void on_btn50_clicked();    // 50원 버튼
-    void on_btn100_clicked();   // 100원 버튼
-    void on_btn500_clicked();   // 500원 버튼
+    void on_btn10_clicked();
+    void on_btn50_clicked();
+    void on_btn100_clicked();
+    void on_btn500_clicked();
 
-    void on_btnCoffee_clicked();  // 커피 버튼
-    void on_btnTea_clicked();     // 차 버튼
-    void on_btnMilk_clicked();    // 우유 버튼
-    void on_btnReset_clicked();   // 리셋 버튼
+    void on_btnCoffee_clicked();
+    void on_btnTea_clicked();
+    void on_btnMilk_clicked();
+    void on_btnReset_clicked();
 
 private:
     Ui::Widget *ui;
-    int money = 0;  // 현재 금액
-    void updateButtons();  // 버튼 활성화/비활성화 업데이트
+    int money = 0;
+    void updateButtons();
 };
 
 #endif
